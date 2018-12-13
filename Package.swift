ca2e21cf36406ec6fb3bd090cc437faff3cc13f4
Mihael Isaev <isaev.mihael@gmail.com>
2018-12-14 00:40:17 +0400

    Added Auth lib

--- Package.swift
+++ Package.swift
@@ -7,9 +7,10 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0"),
+        .package(url: "https://github.com/vapor/auth.git", from:"2.0.0"),
     ],
     targets: [
-        .target(name: "App", dependencies: ["FluentPostgreSQL", "Vapor"]),
+        .target(name: "App", dependencies: ["Authentication", "FluentPostgreSQL", "Vapor"]),
         .target(name: "Run", dependencies: ["App"]),
         .testTarget(name: "AppTests", dependencies: ["App"])
     ]
