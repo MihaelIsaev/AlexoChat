33ff8b56f53df2c562cfa912a1b6c115b3efb7a3
Mihael Isaev <isaev.mihael@gmail.com>
2018-12-23 09:16:37 +0400

    Add FIXME marks

--- Sources/App/WS/WSController.swift
+++ Sources/App/WS/WSController.swift
@@ -1,22 +1,24 @@
-//
-//  WSController.swift
-//  WS
-//
-//  Created by Mihael Isaev on 22/12/2018.
-//
-
 import Foundation
+import Vapor
 import WS
+import FluentQuery
 
 class WSController: WSBindController {
-    override init() {
-        super.init()
-        bind(.message, message)
-        bind(.typing, typing)
-    }
-    
     override func onOpen(_ client: WSClient) {
-        
+        guard let user: User = try? client.req.requireAuthenticated() else { print("user not authenticated"); return }
+        _ = client.req.requestPooledConnection(to: .psql).flatMap { conn -> Future<Void> in
+            let fq = FQL().select(all: Room.self).from(Room.self)
+            fq.where(\Room.deletedAt == nil && \Room.members ~~ [user.id!]) //FIXME: ~~ operator doesn't work, should use SwifQL lib instead
+            return try fq.execute(on: conn, andDecode: Room.self).map { rooms in
+                var channels = rooms.compactMap { $0.id?.uuidString }
+                channels.append(user.id!.uuidString)
+                client.subscribe(to: channels)
+            }
+        }.catchMap { error in
+            print("subscribe error: \(error)")
+        }.always {
+            print("connected user subscribed")
+        }
     }
     override func onClose(_ client: WSClient) {}
 }
