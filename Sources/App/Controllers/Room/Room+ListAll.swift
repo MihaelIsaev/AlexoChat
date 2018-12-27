784b012729579acd60fb0b3739e2097ce4d56d6b
Mihael Isaev <isaev.mihael@gmail.com>
2018-12-28 02:59:33 +0400

    Getting rooms now accept limit/offset query params

--- Sources/App/Controllers/Room/Room+ListAll.swift
+++ Sources/App/Controllers/Room/Room+ListAll.swift
@@ -4,8 +4,10 @@ import FluentQuery
 
 extension RoomController {
     func listAll(_ req: Request) throws -> Future<[Room]> {
+        let limit = (try? req.query.get(Int.self, at: "limit")) ?? 20
+        let offset = (try? req.query.get(Int.self, at: "offset")) ?? 0
         let user = try req.requireAuthenticated(User.self)
         guard user.isAdmin == true else { throw Abort(.forbidden) }
-        return Room.query(on: req).all()
+        return Room.query(on: req).range(lower: limit, upper: limit + offset).all()
     }
 }
--- Sources/App/Controllers/Room/Room+ListMy.swift
+++ Sources/App/Controllers/Room/Room+ListMy.swift
@@ -5,6 +5,8 @@ import FluentQuery
 extension RoomController {
     func listMy(_ req: Request) throws -> Future<[Room]> {
         let user = try req.requireAuthenticated(User.self)
+        let limit = (try? req.query.get(Int.self, at: "limit")) ?? 20
+        let offset = (try? req.query.get(Int.self, at: "offset")) ?? 0
         guard let userId = user.id else { throw Abort(.internalServerError) }
         return req.requestPooledConnection(to: .psql).flatMap { conn in
             defer { try? req.releasePooledConnection(conn, to: .psql) }
@@ -12,6 +14,8 @@ extension RoomController {
             fq.select(all: Room.self)
             fq.from(Room.self)
             fq.where(\Room.deletedAt == nil && FQWhere("members @> ARRAY['\(userId.uuidString)']::uuid[]")) //FIXME: ~~ operator doesn't work, should use SwifQL lib instead
+            fq.limit(limit)
+            fq.offset(offset)
             return try fq.execute(on: conn, andDecode: Room.self)
         }
     }
