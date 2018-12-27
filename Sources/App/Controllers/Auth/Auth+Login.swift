450c5721d78099fb5f9b02953270468b3cabfe9d
Mihael Isaev <isaev.mihael@gmail.com>
2018-12-28 02:32:48 +0400

    Return `AuthResponse` model instead of just token string

--- Sources/App/Controllers/Auth/Auth+Login.swift
+++ Sources/App/Controllers/Auth/Auth+Login.swift
@@ -1,9 +1,16 @@
 import Vapor
 
 extension AuthController {
-    func login(_ req: Request) throws -> Future<Token> {
+    struct AuthResponse: Content {
+        var token, nickname: String
+        var isAdmin: Bool
+    }
+    
+    func login(_ req: Request) throws -> Future<AuthResponse> {
         let user = try req.requireAuthenticated(User.self)
         let token = try Token.generate(for: user)
-        return token.save(on: req)
+        return token.save(on: req).map { token in
+            return AuthResponse(token: token.token, nickname: user.nickname, isAdmin: user.isAdmin == true)
+        }
     }
 }
